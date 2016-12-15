b25b426b0e2aa34fb3449951a9ab815cd0da410d
Bartek <bartulizna@gmail.com>
2016-12-15 13:46:46 +0100

    no kurwa SPI

--- lab7/main.c
+++ lab7/main.c
@@ -1,22 +1,43 @@
 #include "AT91SAM9263.h"
 
 #define CLOCK 1 << 14
-#define DATA 1 << 12
+#define DATA (1 << 12)
 #define CS 1 << 15
 #define SLEEP_TIME 1000
 #define MS 6250
 #define PITS_FLAG 1<<0
+#define PITEN (1<<24)
+#define PITIEN (1<<25)
+#define BAUDRATE 115200
+#define MCK 100000000
+#define DIVISOR 16
+#define ALL_INTERRUPTS 0xFFFFFFFF
+#define TEMP_FACTOR 0.0625
 
-int temperature;
+int sprintf(char * buffer,const char * format,...);
+
+int temperature = 0;
+float realTemperature;
 
 void dbgu_print_ascii(const char *a) {}
+
+void setClock()
+{
+	AT91C_BASE_SYS -> SYS_PIOB_SODR = CLOCK;
+}
+
+void clearClock()
+{
+	AT91C_BASE_SYS -> SYS_PIOB_CODR = CLOCK;
+}
+
 
 void clearPITS()
 {
 	AT91C_BASE_PITC-> PITC_PIVR;//clears a PITS flag
 }
 
-void DBGU_init(void)
+void initializeDBGU(void)
 {
 	AT91C_BASE_DBGU->DBGU_IDR = ALL_INTERRUPTS;//deactivate all interrupts
 	AT91C_BASE_DBGU->DBGU_CR = AT91C_US_RSTRX | AT91C_US_RXDIS;//reset and turn off receiver
@@ -43,13 +64,15 @@ void initializeSPI()
 	AT91C_BASE_SYS -> SYS_PIOB_PER = CS;
 	AT91C_BASE_SYS -> SYS_PIOB_OER= DATA;
 	AT91C_BASE_SYS -> SYS_PIOB_ODR= CLOCK;
+	AT91C_BASE_SYS -> SYS_PIOB_ODR= CS;
+	AT91C_BASE_SYS -> SYS_PIOB_SODR= CS;
+	clearClock();
 }
 
 void sendChar(char letter)
 {
 	while(!(AT91C_BASE_DBGU->DBGU_CSR & AT91C_US_TXRDY));
 	AT91C_BASE_DBGU->DBGU_THR = (unsigned int)letter;
-
 }
 
 void displayString(char* string)
@@ -65,55 +88,65 @@ void displayString(char* string)
 void sleep(unsigned int iterations)
 {	
 	unsigned int i;
-	for(i =0; i < 200; ++i)
+    AT91C_BASE_PITC -> PITC_PIMR |= PITEN;
+    for (i=0; i<iterations; i++)
+    {
+        while(!(AT91C_BASE_PITC -> PITC_PISR & 1<<0)){}
+        clearPITS();
+    }
+    AT91C_BASE_PITC -> PITC_PIMR &= ~PITEN;
+}
+
+
+void readSO(int bitNumber)
+{
+	if(AT91C_BASE_SYS -> SYS_PIOB_PDR & DATA)
+		temperature |= 1 << bitNumber;
+	else
+		temperature |= ~(1 << bitNumber);
+}
+
+void readTemperature()
+{
+	int i;
+	AT91C_BASE_SYS -> SYS_PIOB_CODR= CS;
+	for(i = 0; i < 16; ++i)
 	{
+		setClock();
+		readSO(15-i);
+		clearClock();
+		sleep(1);
 		
-		*ptrPIT_MR = PITEN;
-		while(1)
-		{
-			if(*ptrPIT_SR & PT_REACHED)
-				break;
-		}
 	}
+	AT91C_BASE_SYS -> SYS_PIOB_SODR= CS;
+	temperature = temperature >> 3;
+	realTemperature = temperature * TEMP_FACTOR;
+	sleep(SLEEP_TIME);
 }
 
-void setClock()
+void displayTemperature()
 {
-	AT91C_BASE_SYS -> SYS_PIOB_SODR = CLOCK;
+	char tempChar[8];
+	sprintf(tempChar, "%f", realTemperature);
+	displayString(tempChar);
 }
 
-void clearClock()
+void manageLED()
 {
-	AT91C_BASE_SYS -> SYS_PIOB_CODR = CLOCK;
-}
-
-void readSO()
-{
-	int bit;
 	
-	bit = AT91C_BASE_SYS -> SYS_PIOB_PDR= DATA;
-		
-		
-
 }
 
 int main()
 {
     initializeSPI();
     initializePIT();
-    initPIT_IRQ();
-	int i;
+    initializeDBGU();
+    
     while(1)
     {
-    	for(i = 0; i < 16; ++i)
-    	{
-    		setClock();
-    		readSO();
-    		clearClock();
-    		sleep(1);
-    	}
-    	displayString(temperature);
-    	sleep(SLEEP_TIME);
+    	readTemperature();
+    	displayTemperature();
+    	manageLED();
     }
 }
  
